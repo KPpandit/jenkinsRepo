6f3e64632521a50431dec23e299eda8a8e70a292
Krishna Purohit <purohitkrishna02@gmail.com>
2025-02-26 11:30:55 +0530

    second commit

--- src/main/java/com/jenkins/jenkins/JenkinsApplication.java
+++ src/main/java/com/jenkins/jenkins/JenkinsApplication.java
@@ -1,12 +1,22 @@
 package com.jenkins.jenkins;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsApplication {
 
+public  static 	Logger LOGGER= LoggerFactory.getLogger(JenkinsApplication.class);
+		@PostConstruct
+		public void innt(){
+			LOGGER.info("Applciation Started");
+		}
+
 	public static void main(String[] args) {
+			LOGGER.info("Application executed ...");
 		SpringApplication.run(JenkinsApplication.class, args);
 	}
 
--- src/test/java/com/jenkins/jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/jenkins/jenkins/JenkinsApplicationTests.java
@@ -1,13 +1,18 @@
 package com.jenkins.jenkins;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinsApplicationTests {
-
+	public  static Logger LOGGER= LoggerFactory.getLogger(JenkinsApplicationTests.class);
 	@Test
 	void contextLoads() {
+		LOGGER.info("Test Case Executing ...");
+		LOGGER.info("Test Case Executing Second Log Statement ...");
+		assert (true);
 	}
 
 }
