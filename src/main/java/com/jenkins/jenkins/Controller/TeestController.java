24b7c798cf9bef77098ef820d2365798893825cd
Krishna Purohit <purohitkrishna02@gmail.com>
2025-02-27 00:36:42 +0530

    commit by for main branch

--- src/main/java/com/jenkins/jenkins/Controller/TeestController.java
+++ src/main/java/com/jenkins/jenkins/Controller/TeestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TeestController {
     @GetMapping("/api/name")
     public String name(){
-        return "Krishna Purohit";
+        return "Krishna Purohit123";
     }
 }
